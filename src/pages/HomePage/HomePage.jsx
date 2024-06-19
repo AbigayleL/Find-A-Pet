7634294dd3bc9a16902ae283ab4925d24503fb46
Liu Ladniak <liuladniak@gmail.com>
2024-06-19 16:50:15 -0400

    styels

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,9 @@
 import Hero from "../../components/Hero/Hero";
-import Header from "../../components/Header/Header";
 import "./HomePage.scss";
 
 function HomePage() {
   return (
     <main>
-      <Header />
       <Hero />
     </main>
   );
